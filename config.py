07a8bab71fdc755ddc927e71a51d4d9c020f7db4
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-03-06 19:23:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import logging
 
-TOKEN = "6348055484:AAGhOv6sx5B4acQj-XUfRWV3kJz53FvioWs" #TasuAdmin
 #TOKEN = 'TOKEN' #token produzione
 
 admin_id = 1026795763
@@ -13,7 +12,7 @@ use_local_api = False  # Set to False if the API is not in local
 
 log_level = logging.INFO  # logger type: INFO, ERROR, DEBUG
 
-leonardo_token = '528d01a5-88f3-4d25-8cd4-6411d4b3d819'
+leonardo_token = ''
 
 
 
